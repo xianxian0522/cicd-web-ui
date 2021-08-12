41ef206ae1a90acc62f1a5c953b557c0373347ff
sheng.xu <sheng.xu@sumscope.com>
2021-08-12 22:13:52 +0800

    add path

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 npm config set registry=http://172.16.97.41:9000/repository/npm/
 npm i
-npm run build --prod
+export PATH=$(pwd)/node_modules/.bin:$PATH
+npm run build
 
 mv dist $APP
 tar -czvf "$APP-$VERSION_NAME.tar.gz" $APP/
