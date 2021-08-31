1da8ca8409ecf7447dfc6cf61269c101fd963442
suixian.gong <suixian.gong@sumscope.com>
2021-08-31 17:28:15 +0800

    overview

---
+++ src/components/CommonCode.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="overview-panel-content flex-1">
+    <div class="overview-measures-row flex">
+      <div class="overview-panel-big-padded flex-1 small flex-center big-spacer-left">
+        <a class="overview-measures-value text-light">0</a>
+        <icon-font type="icon-bug" class="icon-svg little-spacer-right big-spacer-left" />
+        <span>New Bugs</span>
+      </div>
+      <div class="overview-panel-big-padded overview-measures-aside flex-center"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {createFromIconfontCN} from "@ant-design/icons-vue";
+
+const IconFont = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_2780490_4v25w6tlomw.js'
+})
+
+export default {
+  name: "CommonCode",
+  components: {IconFont},
+  setup() {
+
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+@import "index";
+
+.overview-panel-content {
+  background: #fff;
+  border: 1px solid #e6e6e6;
+}
+.text-light {
+  font-weight: 300;
+}
+.overview-panel-big-padded {
+  padding: 24px;
+
+  a {
+    border-bottom: 1px solid #cae3f2;
+    color: #236a97;
+    cursor: pointer;
+    outline: 0;
+    text-decoration: none;
+    transition: all .2s ease;
+  }
+}
+.overview-measures-aside {
+  flex-basis: 200px;
+  box-sizing: border-box;
+}
+.overview-measures-row {
+  min-height: 105px;
+  box-sizing: border-box;
+}
+.big-spacer-left {
+  margin-left: 16px;
+}
+.overview-measures-value {
+  line-height: 1;
+  font-size: 36px;
+  white-space: nowrap;
+}
+.icon-svg {
+  font-size: 16px
+}
+.little-spacer-right {
+  margin-right: 4px;
+}
+</style>
--- src/views/OverviewComponent.vue
+++ src/views/OverviewComponent.vue
@@ -23,7 +23,17 @@
       <div class="flex-1">
         <div class="overview-panel">
           <h2 class="overview-panel-title">Measures</h2>
-          <div></div>
+          <div class="flex">
+            <div class="overview-btn" v-for="btn in buttonMenu" :key="btn.id" @click="handleActiveChange(btn.id)" :class="{'btn-active': btn.id === activeKey}">
+              <div :class="{'btn-select': btn.id === activeKey}"></div>
+              <div class="text-left overview-measures-tab">
+                <div class="text-bold">{{ btn.title }}</div>
+                <div v-if="btn.id === 1" class="note spacer-top">Since August 16, 2021</div>
+                <div v-if="btn.id === 1" class="note little-spacer-top">Started 15 days ago</div>
+              </div>
+            </div>
+          </div>
+          <CommonCode />
         </div>
       </div>
     </div>
@@ -32,14 +42,35 @@
 
 <script lang="ts">
 import {QuestionCircleOutlined} from '@ant-design/icons-vue'
+import {ref} from "vue";
+import CommonCode from "@/components/CommonCode.vue";
 
 export default {
   name: "OverviewComponent",
-  components: { QuestionCircleOutlined },
+  components: { QuestionCircleOutlined, CommonCode },
+  setup() {
+    const activeKey = ref<number>(1)
+    const buttonMenu = ref([
+      {id: 1, title: 'New Code'},
+      {id: 2, title: 'Overall Code'},
+    ])
+
+    const handleActiveChange = (index: number) => {
+      activeKey.value = index
+    }
+
+    return {
+      activeKey,
+      buttonMenu,
+      handleActiveChange,
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
+@import "../components/index";
+
 .overview {
   padding: 20px;
 }
@@ -54,15 +85,8 @@ export default {
 .big-spacer-right {
   margin-right: 16px;
 }
-.flex-1 {
-  flex: 1;
-}
 .overview-panel {
   min-height: 100%;
-}
-.flex-center {
-  display: flex;
-  align-items: center;
 }
 .overview-panel-title {
   text-transform: uppercase;
@@ -96,7 +120,58 @@ export default {
 .huge {
   font-size: 24px;
 }
-.small, small {
+.btn-active {
+  background-color: #fff;
+}
+.overview-btn {
+  position: relative;
+  border-top: 1px solid #e6e6e6;
+  border-left: 1px solid #e6e6e6;
+  border-right: none;
+  border-bottom: none;
+  margin-bottom: -1px;
+  min-width: 128px;
+  min-height: 56px;
+  height: auto;
+  outline: 0;
+  padding: calc(16px);
+}
+.overview-btn:last-child {
+  border-right: 1px solid rgb(230, 230, 230);
+}
+.overview-btn:not(.btn-active) {
+  cursor: pointer;
+}
+.overview-btn:not(.btn-active):hover {
+  background-color: #f8f8f8;
+}
+.btn-select {
+  display: block;
+  background-color: rgb(75, 159, 213);
+  height: 3px;
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: -1px;
+}
+.overview-measures-tab {
+  width: 128px;
+}
+.text-left {
+  text-align: left;
+}
+.text-bold {
+  font-weight: 700;
+}
+.spacer-top {
+  margin-top: 8px;
+}
+.note {
+  color: #666;
   font-size: 12px;
 }
+.little-spacer-top {
+  margin-top: 4px;
+}
+
 </style>
