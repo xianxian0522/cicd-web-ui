44170134f87fd030d32e79e3c931146da3d5a897
suixian.gong <suixian.gong@sumscope.com>
2021-08-31 14:04:41 +0800

    sonar

---
+++ src/views/OverviewComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    over
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "OverviewComponent"
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
--- src/views/SonarQube.vue
+++ src/views/SonarQube.vue
@@ -1,34 +1,62 @@
 <template>
   <div>
-    <div class="common-sonar">
-      <div class="sonar-header">
-        <a-button @click="goToBack"><ArrowLeftOutlined /></a-button>
-        <div>{{ projectKey }}</div>
+    <div class="sonar-content-header">
+      <div class="common-sonar">
+        <div class="sonar-header">
+          <a-button @click="goToBack"><ArrowLeftOutlined /></a-button>
+          <div>{{ projectKey }}</div>
+        </div>
+        <div>
+          <ul class="sonar-menu-bar">
+            <li v-for="menu in menuBar" :key="menu.id" @click="handleActive(menu.id)">
+              <a :class="{active: menu.id === activeKey}">{{ menu.title }}</a>
+            </li>
+          </ul>
+        </div>
       </div>
+    </div>
+    <div>
+      <OverviewComponent v-if="activeKey === 1" />
+      <IssuesComponent v-if="activeKey === 2" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import {useRoute, useRouter} from "vue-router";
-import {ref} from "vue";
-import {ArrowLeftOutlined} from '@ant-design/icons-vue'
+import {defineAsyncComponent, ref} from "vue";
+import {ArrowLeftOutlined} from '@ant-design/icons-vue';
 
 export default {
   name: "SonarQube",
-  components: { ArrowLeftOutlined },
+  components: {
+    ArrowLeftOutlined,
+    OverviewComponent: defineAsyncComponent(() => import('./OverviewComponent.vue')),
+    IssuesComponent: defineAsyncComponent(() => import('./IssuesComponent.vue')),
+  },
   setup() {
     const router = useRouter()
     const route = useRoute()
     const projectKey = ref(route.query.projectKey)
+    const activeKey = ref<number>(1)
+    const menuBar = ref([
+      { id: 1, title: 'Overview' },
+      { id: 2, title: 'Issues' }
+    ])
 
     const goToBack = () => {
       router.back()
     }
+    const handleActive = (index: number) => {
+      activeKey.value = index
+    }
 
     return {
+      activeKey,
+      menuBar,
       projectKey,
       goToBack,
+      handleActive,
     }
   }
 }
@@ -46,5 +74,35 @@ export default {
 .common-sonar {
   background: #fff;
   padding: 0 20px;
+}
+.sonar-content-header {
+  border-bottom: 1px solid #e6e6e6;
+}
+.sonar-menu-bar {
+  display: flex;
+  align-items: center;
+  margin-top: 8px;
+  margin-bottom: 0;
+  padding: 0;
+  list-style: none;
+  li {
+    cursor: pointer;
+    a {
+      display: block;
+      height: 24px;
+      line-height: 16px;
+      padding-top: 2px;
+      border-bottom: 3px solid transparent;
+      box-sizing: border-box;
+      color: #333;
+      transition: none;
+    }
+  }
+}
+.sonar-menu-bar>li>a.active, .sonar-menu-bar>li>a:focus, .sonar-menu-bar>li>a:hover {
+  border-bottom-color: #4b9fd5;
+}
+.sonar-menu-bar li+li {
+  margin-left: 20px;
 }
 </style>
